046f5028256e840ccb1668db5956a5813979813e
Waysyy <annatkach225@gmail.com>
2022-09-16 19:43:58 +0700

    ~.~

---
+++ LanguagesLab/LanguagesLab/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace LanguagesLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.programmingLanguageBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.XML = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.programmingLanguageBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // programmingLanguageBindingSource
+            // 
+            this.programmingLanguageBindingSource.DataSource = typeof(LanguagesLab.Form1.ProgrammingLanguage);
+            // 
+            // XML
+            // 
+            this.XML.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.XML.Location = new System.Drawing.Point(576, 174);
+            this.XML.Name = "XML";
+            this.XML.Size = new System.Drawing.Size(182, 95);
+            this.XML.TabIndex = 1;
+            this.XML.Text = "XML";
+            this.XML.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.XML.UseVisualStyleBackColor = true;
+            this.XML.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(101, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(409, 240);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.XML);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.programmingLanguageBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource programmingLanguageBindingSource;
+        private System.Windows.Forms.Button XML;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ LanguagesLab/LanguagesLab/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LanguagesLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e){}
+        public List<ProgrammingLanguage> listLang2 = new List<ProgrammingLanguage>();
+        public static class Deserialization
+        {
+            public class Languages : Elements
+            {
+                public List<ProgrammingLanguage> listLang = new List<ProgrammingLanguage>();
+                public override List<ProgrammingLanguage> RetList()
+                {                                 
+                    var xDoc = XDocument.Load(Environment.CurrentDirectory + "\\lang.xml");
+                    foreach (var data in xDoc.Element("languages").Elements("lang"))
+                    {
+                        listLang.Add(new ProgrammingLanguage { Name = data.Attribute("name").Value, Appeared = Convert.ToInt32(data.Element("appeared").Value), Creator = data.Element("creator").Value });                       
+                    }
+                    return listLang;
+                }          
+            }
+        }
+        public class ProgrammingLanguage
+        {
+            public string Name { get; set; }
+            public int Appeared { get; set; }
+            public string Creator { get; set; }            
+        }
+        public abstract class Elements
+        {
+            public abstract List<ProgrammingLanguage> RetList();               
+        }
+        public void SetDataGrid()
+        {
+            Deserialization.Languages dt = new Deserialization.Languages();
+
+            dataGridView1.Columns.Add("Column1", "Name");
+            dataGridView1.Columns.Add("Column2", "Appeared");
+            dataGridView1.Columns.Add("Column3", "Creator");
+                        
+            listLang2 = (dt.RetList());
+            foreach (var i in listLang2)
+            {
+                dataGridView1.Rows.Add(i.Name, i.Appeared, i.Creator);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            SetDataGrid();
+        }
+    }
+}
